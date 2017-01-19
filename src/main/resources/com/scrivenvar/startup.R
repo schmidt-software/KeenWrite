ae974595685ed4d09203856c1d3d3ff1fb8994da
djarvis <dave.jarvis@gmail.com>
2017-01-18 18:04:54 -0800

    Emptied the R startup script.

--- src/main/resources/com/scrivenvar/startup.R
+++
@@ -1,5 +0,0 @@
-assign( 'anchor', as.Date( '$date.anchor$', format='%Y-%m-%d' ), envir = .GlobalEnv );
-setwd( '$application.r.working.directory$' );
-source( '../bin/pluralize.R' );
-source( '../bin/csv.R' );
-source( '../bin/common.R' );
