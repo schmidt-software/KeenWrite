5230b6004b42de3b30174e8fad1a67af9895d114
DaveJarvis <Dave.Jarvis@gmail.com>
2021-02-01 23:50:49 -0800

    Remove archive and revert TLS validation

--- src/main/java/com/keenwrite/io/HttpMediaType.java
+++ src/main/java/com/keenwrite/io/HttpMediaType.java
@@ -1,20 +1,12 @@
 /* Copyright 2020-2021 White Magic Software, Ltd. -- All rights reserved. */
 package com.keenwrite.io;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
-import static com.keenwrite.Constants.DIAGRAM_SERVER_NAME;
 import static com.keenwrite.events.StatusEvent.clue;
 import static com.keenwrite.io.MediaType.UNDEFINED;
 import static java.net.http.HttpClient.Redirect.NORMAL;
@@ -32,7 +24,6 @@ public final class HttpMediaType {
     .newBuilder()
     .connectTimeout( ofSeconds( 5 ) )
     .followRedirects( NORMAL )
-    .sslContext( createSslContext() )
     .build();
 
   /**
@@ -82,50 +73,5 @@ public final class HttpMediaType {
     }
 
     return mediaType[ 0 ];
-  }
-
-  private static SSLContext createSslContext() {
-    try {
-      final var context = SSLContext.getInstance( "TLS" );
-      context.init(
-        new KeyManager[ 0 ],
-        new TrustManager[]{new DefaultTrustManager()},
-        new SecureRandom() );
-      SSLContext.setDefault( context );
-      return context;
-    } catch( final Exception ex ) {
-      clue( ex );
-      throw new RuntimeException( ex );
-    }
-  }
-
-  private static class DefaultTrustManager implements X509TrustManager {
-
-    @Override
-    public void checkServerTrusted(
-      final X509Certificate[] chain, final String authType )
-      throws CertificateException {
-      for( final var cert : chain ) {
-        for( final var names : cert.getSubjectAlternativeNames() ) {
-          for( final var name : names ) {
-            if( DIAGRAM_SERVER_NAME.equalsIgnoreCase( name.toString() ) ) {
-              return;
-            }
-          }
-        }
-      }
-
-      clue( "Main.status.image.request.error.cert", DIAGRAM_SERVER_NAME );
-      throw new CertificateException();
-    }
-
-    @Override
-    public void checkClientTrusted(
-      final X509Certificate[] chain, final String authType ) {}
-
-    @Override
-    public X509Certificate[] getAcceptedIssuers() {
-      return null;
-    }
   }
 }
