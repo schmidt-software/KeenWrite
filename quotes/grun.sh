502c0812179960447ef6e7159493d916f30076bf
djarvis <dave.jarvis@gmail.com>
2021-05-02 10:26:05 -0700

    Ignore build artefacts

--- quotes/grun.sh
+++ quotes/grun.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 CLASSPATH=".:antlr.jar"
+ARG_FILENAME="$1"
+
+if [ -z "$ARG_FILENAME" ]; then
+  ARG_FILENAME="english.txt"
+fi
+
 java -Xmx500M -cp $CLASSPATH "org.antlr.v4.Tool" "English.g4"
 javac -cp $CLASSPATH *.java
 java -Xmx500M -cp $CLASSPATH "org.antlr.v4.gui.TestRig" English document \
-  -tree "english.txt"
+  -tree "$ARG_FILENAME"
 
