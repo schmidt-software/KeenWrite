e848ed2622a140f8651d891ae2a33d56ca4ad48b
DaveJarvis <Dave.Jarvis@gmail.com>
2022-01-06 18:48:49 -0800

    Add Monokai theme

--- src/main/java/com/keenwrite/preferences/SkinProperty.java
+++ src/main/java/com/keenwrite/preferences/SkinProperty.java
@@ -24,6 +24,7 @@ public final class SkinProperty extends SimpleObjectProperty<String> {
     sSkins.add( "Count Darcula" );
     sSkins.add( "Haunted Grey" );
     sSkins.add( "Modena Dark" );
+    sSkins.add( "Monokai" );
     sSkins.add( SKIN_DEFAULT );
     sSkins.add( "Silver Cavern" );
     sSkins.add( "Solarized Dark" );
