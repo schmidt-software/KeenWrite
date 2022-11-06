6e94bb23b9036257771c1e29db9be89f35811fb7
DaveJarvis <Dave.Jarvis@gmail.com>
2022-11-06 14:25:40 -0800

    Update copyright notice, formatting

--- R/pluralize.R
+++ R/pluralize.R
@@ -1,9 +1,11 @@
 # -----------------------------------------------------------------------------
-# Copyright 2021 by Robin Gertenbach
+# Copyright 2021 Robin Gertenbach.
+#
+# Copyright 2021 White Magic Software, Ltd.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files ( the
-# "Software" ), to deal in the Software without restriction, including
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
 # without limitation the rights to use, copy, modify, merge, publish,
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
