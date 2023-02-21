181293ff44133d71337952afaaa2a5815c9a33de
DaveJarvis <Dave.Jarvis@gmail.com>
2023-02-20 18:01:11 -0800

    Fix pluralization unit test

--- src/test/java/com/keenwrite/r/PluralizeTest.java
+++ src/test/java/com/keenwrite/r/PluralizeTest.java
@@ -19,23 +19,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class PluralizeTest {
   private static final ScriptEngine ENGINE =
-      new ScriptEngineManager().getEngineByName( "Renjin" );
+    new ScriptEngineManager().getEngineByName( "Renjin" );
 
   private static final Map<String, String> PLURAL_MAP = ofEntries(
-      entry( "beef", "beefs" ),
-      entry( "brother", "brothers" ),
-      entry( "child", "children" ),
-      entry( "cow", "cows" ),
-      entry( "ephemeris", "ephemerides" ),
-      entry( "genie", "genies" ),
-      entry( "money", "moneys" ),
-      entry( "mongoose", "mongooses" ),
-      entry( "mythos", "mythoi" ),
-      entry( "octopus", "octopuses" ),
-      entry( "ox", "oxen" ),
-      entry( "soliloquy", "soliloquies" ),
-      entry( "trilby", "trilbys" ),
-      entry( "wolf", "wolves" )
+    entry( "beef", "beefs" ),
+    entry( "brother", "brothers" ),
+    entry( "child", "children" ),
+    entry( "cow", "cows" ),
+    entry( "ephemeris", "ephemerides" ),
+    entry( "genie", "genies" ),
+    entry( "money", "moneys" ),
+    entry( "mongoose", "mongooses" ),
+    entry( "mythos", "mythoi" ),
+    entry( "octopus", "octopuses" ),
+    entry( "ox", "oxen" ),
+    entry( "soliloquy", "soliloquies" ),
+    entry( "trilby", "trilbys" ),
+    entry( "wolf", "wolves" )
   );
 
   @BeforeAll
@@ -45,14 +45,14 @@ public class PluralizeTest {
   }
 
   @Test
-  @SuppressWarnings("UnnecessaryLocalVariable")
   public void test_Pluralize_SingularForms_PluralForms()
-      throws ScriptException {
-    for( final var key : PLURAL_MAP.keySet() ) {
-      final var expectedSingular = key;
-      final var expectedPlural = PLURAL_MAP.get( key );
-      final var actualSingular = pluralize( key, 1 );
-      final var actualPlural = pluralize( key, 2 );
+    throws ScriptException {
+
+    for( final var entry : PLURAL_MAP.entrySet() ) {
+      final var expectedSingular = entry.getKey();
+      final var expectedPlural = entry.getValue();
+      final var actualSingular = pluralize( expectedSingular, 1 );
+      final var actualPlural = pluralize( expectedSingular, 2 );
 
       assertEquals( expectedSingular, actualSingular );
       assertEquals( expectedPlural, actualPlural );
@@ -60,8 +60,9 @@ public class PluralizeTest {
   }
 
   private String pluralize( final String word, final int count )
-      throws ScriptException {
-    return r( format( "pluralize( '%s', %d );", word, count ) ).toString();
+    throws ScriptException {
+    final var stmt = format( "pluralize( word='%s', n=%d );", word, count );
+    return r( stmt ).toString();
   }
 
   private static Object r( final String code ) throws ScriptException {
