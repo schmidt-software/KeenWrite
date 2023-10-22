49bcfa3756a1a5fadbaa0db81b7371da1391545a
DaveJarvis <thangalin@gmail.com>
2023-10-22 12:37:40 -0700

    Adds missing shell script for Java launcher

---
+++ keenwrite.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+java \
+  --add-opens=javafx.controls/javafx.scene.control=ALL-UNNAMED \
+  --add-opens=javafx.controls/javafx.scene.control.skin=ALL-UNNAMED \
+  --add-opens=javafx.graphics/javafx.scene.text=ALL-UNNAMED \
+  --add-opens=javafx.graphics/com.sun.javafx.css=ALL-UNNAMED \
+  --add-opens=javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED \
+  --add-opens=javafx.graphics/com.sun.javafx.text=ALL-UNNAMED \
+  --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.application=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.geom=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.text=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.scene.text=ALL-UNNAMED \
+  --add-exports=javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED \
+  -jar keenwrite.jar $@
+
