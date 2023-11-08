8a68ee2a8265efcaa753d8ada6213406858f20e1
DaveJarvis <thangalin@gmail.com>
2023-11-07 23:44:46 -0800

    Adds counter for file downloads

---
+++ www/downloads/counter.php
@@ -0,0 +1,248 @@
+<?php
+  // Log all errors to a file.
+  ini_set( "log_errors", 1 );
+  ini_set( "error_log", "/tmp/php-errors.log" );
+
+  // Keep running upon client disconnect (helps catch file transfer failures).
+  ignore_user_abort( true );
+
+  // Allow the download to complete.
+  set_time_limit( 0 );
+
+  // Allow setting session variables (cookies).
+  session_start();
+
+  /**
+   * Answers whether the user's session has expired.
+   *
+   * @param int $lifetime Number of seconds the session lasts before expiring.
+   *
+   * @return bool True indicates the session has expired (or was not set).
+   */
+  function session_expired( $lifetime ) {
+    $now = time();
+    $expired = !isset( $_SESSION[ 'LAST_ACTIVITY' ] );
+
+    if( !$expired && ($now - $_SESSION[ 'LAST_ACTIVITY' ]) > $lifetime ) {
+      $_SESSION = array();
+
+      session_destroy();
+
+      $expired = true;
+    }
+
+    // Update last activity timestamp.
+    $_SESSION[ 'LAST_ACTIVITY' ] = $now;
+
+    return $expired;
+  }
+
+  function create_lock_filename( $filename ) {
+    return $filename .'.lock';
+  }
+
+  /**
+   * Acquires a lock for a particular file. Callers would be prudent to
+   * call this function from within a try/finally handler and close the lock
+   * in the finally block. The amount of time between opening and closing
+   * the lock must be quick because parallel processes will be waiting on
+   * the lock's release.
+   *
+   * @param string $filename The name of file to lock.
+   *
+   * @return bool True if the lock was obtained, false upon excessive attempts.
+   */
+  function lock_open( $filename ) {
+    $lockfile = create_lock_filename( $filename );
+
+    // Track the number of times a lock attempt is made.
+    $iterations = 0;
+
+    do {
+      // Create and test lock file existence atomically.
+      if( @mkdir( $lockfile, 0777 ) ) {
+        // Exit the loop.
+        $iterations = 0;
+      }
+      else {
+        $iterations++;
+        $lifetime = time() - filemtime( $lockfile );
+
+        if( $lifetime > 10 ) {
+          // If the lock has gone stale, delete it.
+          @rmdir( $lockfile );
+        }
+        else {
+          // Wait a random duration to avoid concurrency conflicts.
+          usleep( rand( 1000, 10000 ) );
+        }
+      }
+    }
+    while( $iterations > 0 && $iterations < 10 );
+
+    // Indicate whether the maximum number of lock attempts were exceeded.
+    return $iterations == 0;
+  }
+
+  function lock_close( $filename ) {
+    @rmdir( create_lock_filename( $filename ) );
+  }
+
+  /**
+   * Increments the number in a file using an exclusive lock. If the file
+   * doesn't exist, it will be created and the initial value set to 0.
+   *
+   * @param string $filename The file containing a number to increment.
+   */
+  function increment_count( $filename ) {
+    try {
+      lock_open( $filename );
+
+      // Coerce value to largest natural numeric data type.
+      $count = file_get_contents( $filename ) + 0;
+
+      // Write the new counter value.
+      file_put_contents( $filename, $count + 1 );
+    }
+    finally {
+      lock_close( $filename );
+    }
+  }
+
+  /**
+   * Increments the number of times a file has been accessed, respecting
+   * session expiration and atomic read/write operations.
+   *
+   * @param string $filename The file containing a number to increment.
+   */
+  function hit_count( $filename ) {
+    if( session_expired( 7 * 24 * 60 * 60 ) ) {
+      increment_count( $filename );
+    }
+  }
+
+  function normalize_filename( $fileinfo ) {
+    $basename = $fileinfo[ 'basename' ];
+
+    if( isset( $_SERVER[ 'HTTP_USER_AGENT' ] ) ) {
+      $periods = substr_count( $basename, '.' );
+
+      // Address IE bug regarding multiple periods in filename.
+      $basename = strstr( $_SERVER[ 'HTTP_USER_AGENT' ], 'MSIE' )
+        ? preg_replace( '/\./', '%2e', $basename, $periods - 1 )
+        : $basename;
+    }
+
+    return $basename;
+  }
+
+  function get_content_type( $fileinfo ) {
+    $extension = strtolower( $fileinfo[ 'extension' ] );
+
+    switch( $extension ) {
+      case 'app': $ctype='application/octet-stream'; break;
+      case 'bin': $ctype='application/octet-stream'; break;
+      case 'exe': $ctype='application/octet-stream'; break;
+      case 'jar': $ctype='application/octet-stream'; break;
+      case 'zip': $ctype='application/zip'; break;
+      case 'avi': $ctype='video/msvideo'; break;
+      case 'mp3': $ctype='audio/mpeg'; break;
+      case 'mpg': $ctype='video/mpeg'; break;
+      case 'mpv': $ctype='video/mpv'; break;
+      case 'webm': $ctype='video/webm'; break;
+      default: $ctype='application/force-download'; break;
+    }
+
+    return $ctype;
+  }
+
+  /**
+   * Downloads a file transfer, allowing for resuming partial downloads.
+   *
+   * @param string $path Fully qualified path of file to download.
+   *
+   * @return bool True if the download succeeded.
+   */
+  function download( $path ) {
+    $size = filesize( $path );
+    $fileinfo = pathinfo( $path );
+    $filename = normalize_filename( $fileinfo );
+    $content_type = get_content_type( $fileinfo );
+    $range = '0-0';
+
+    // Check if http_range is sent by browser or download manager.
+    if( isset( $_SERVER[ 'HTTP_RANGE' ] ) ) {
+      list( $units, $range_orig ) = explode( '=', $_SERVER['HTTP_RANGE'], 2 );
+
+      if( $units == 'bytes' ) {
+        // Multiple ranges could be specified, but only serve the first range.
+        list( $range, $extra_ranges ) = explode( ',', $range_orig, 2 );
+      }
+    }
+
+    // Figure out download piece from range.
+    list( $seek_start, $seek_end ) = explode( '-', $range, 2 );
+
+    // Set start and end based on range, otherwise use defaults.
+    $seek_end = empty( $seek_end )
+      ? $size - 1
+      : min( abs( $seek_end + 0 ), $size - 1 );
+    $seek_start = empty( $seek_start || $seek_end < abs( $seek_start + 0 ) )
+      ? 0
+      : max( abs( $seek_start + 0 ), 0 );
+
+    // Send partial content header if downloading a piece (IE workaround).
+    if( $seek_start > 0 || $seek_end < ($size - 1) ) {
+      header( 'HTTP/1.1 206 Partial Content' );
+    }
+
+    $range_bytes = $seek_start . '-' . $seek_end . '/' . $size;
+
+    if( ob_get_level() == 0 ) {
+      ob_start();
+    }
+
+    header( 'Accept-Ranges: bytes' );
+    header( 'Content-Range: bytes ' . $range_bytes );
+    header( 'Content-Type: ' . $content_type );
+    header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
+    header( 'Content-Length: ' . ($seek_end - $seek_start + 1) );
+
+    $fp = fopen( $path, 'rb' );
+
+    if( $fp !== false ) {
+      fseek( $fp, $seek_start );
+
+      $aborted = false;
+      $total_bytes = $seek_start;
+      $chunk_size = 1024 * 8;
+
+      while( !feof( $fp ) && !$aborted ) {
+        set_time_limit( 0 );
+        print( fread( $fp, $chunk_size ) );
+
+        if( ob_get_level() > 0 ) {
+          ob_flush();
+        }
+
+        flush();
+
+        $total_bytes += $chunk_size;
+        $aborted = connection_aborted();
+      }
+
+      if( ob_get_level() > 0 ) {
+        ob_end_flush();
+      }
+
+      fclose( $fp );
+    }
+
+    // Download succeeded if the total bytes sent exceeds the file size.
+    return $total_bytes >= $size;
+  }
+
+  if( download( 'f.txt' ) ) {
+    hit_count( 'f.txt' );
+  }
+?>
