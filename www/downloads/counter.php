2cf02c3710214f393b1b42ba0d8361fbca4b7390
DaveJarvis <thangalin@gmail.com>
2023-11-11 12:25:39 -0800

    Honours HTTP HEAD requests

--- www/downloads/counter.php
+++ www/downloads/counter.php
@@ -134,13 +134,14 @@
   }
 
   /**
-   * Isolate the file name being downloaded.
+   * Normalize the file name being downloaded.
    *
-   * @param array $fileinfo The result from calling pathinfo.
+   * @param string $path The path to the file.
    *
    * @return string The normalized file name.
    */
-  function normalize_filename( $fileinfo ) {
+  function normalize_filename( $path ) {
+    $fileinfo = pathinfo( $path );
     $basename = $fileinfo[ 'basename' ];
 
     if( isset( $_SERVER[ 'HTTP_USER_AGENT' ] ) ) {
@@ -160,97 +161,16 @@
   }
 
   /**
-   * Determine the content type based on the file name extension, rather
-   * than the file contents. This could be inaccurate, but we'll trust that
-   * the website administrator is posting files whose content reflects the
-   * file name extension.
-   * <p>
-   * If the file name extension is not known, the content type will force
-   * the download (to prevent the browser from trying to play the content
-   * directly).
-   *
-   * @param array $fileinfo The result from calling pathinfo.
-   *
-   * @return string The IANA-defined Media Type for the file name extension.
-   */
-  function get_content_type( $fileinfo ) {
-    $extension = strtolower( $fileinfo[ 'extension' ] );
-
-    switch( $extension ) {
-      case 'app': $ctype='application/octet-stream'; break;
-      case 'bin': $ctype='application/octet-stream'; break;
-      case 'exe': $ctype='application/octet-stream'; break;
-      case 'jar': $ctype='application/octet-stream'; break;
-      case 'zip': $ctype='application/zip'; break;
-      case 'avi': $ctype='video/msvideo'; break;
-      case 'mp3': $ctype='audio/mpeg'; break;
-      case 'mpg': $ctype='video/mpeg'; break;
-      case 'mpv': $ctype='video/mpv'; break;
-      case 'webm': $ctype='video/webm'; break;
-      default: $ctype='application/force-download'; break;
-    }
-
-    return $ctype;
-  }
-
-  /**
    * Downloads a file, allowing for resuming partial downloads.
    *
    * @param string $path Fully qualified path of a file to download.
    *
-   * @return bool True if the download succeeded.
+   * @return bool True if the file was transferred.
    */
-  function download( $path ) {
-    // Don't cache the file stats result.
-    clearstatcache();
-
-    $size = @filesize( $path );
-    $size = $size === false || empty( $size ) ? 0 : $size;
-    $fileinfo = pathinfo( $path );
-    $filename = normalize_filename( $fileinfo );
-    $content_type = get_content_type( $fileinfo );
-    $range = "0-$size";
-
-    // Check if a range is sent by browser or download manager.
-    if( isset( $_SERVER[ 'HTTP_RANGE' ] ) ) {
-      list( $units, $range_orig ) = explode( '=', $_SERVER[ 'HTTP_RANGE' ], 2 );
-
-      if( $units == 'bytes' ) {
-        // Multiple ranges could be specified, but only serve the first range.
-        list( $range, $extra_ranges ) = explode( ',', $range_orig, 2 );
-      }
+  function transmit( $path, $seek_start, $size ) {
+    if( ob_get_level() == 0 ) {
+      ob_start();
     }
-
-    // Figure out download piece from range.
-    list( $seek_start, $seek_end ) = explode( '-', $range, 2 );
-
-    // Set start and end based on range, otherwise use defaults.
-    $seek_end = empty( $seek_end )
-      ? max( $size - 1, 0 )
-      : min( abs( $seek_end + 0 ), $size - 1 );
-    $seek_start = empty( $seek_start || $seek_end < abs( $seek_start + 0 ) )
-      ? 0
-      : max( abs( $seek_start + 0 ), 0 );
-
-    header( 'Pragma: public' );
-    header( 'Expires: -1' );
-    header( 'Cache-Control: public, must-revalidate, post-check=0, pre-check=0' );
-    header( "Content-Disposition: attachment; filename=\"$filename\"" );
-
-    $content_length = $size;
-
-    // Send partial content header if downloading a piece (IE workaround).
-    if( $seek_start > 0 || $seek_end < ($size - 1) ) {
-      $range_bytes = $seek_start . '-' . $seek_end . '/' . $size;
-      $content_length = $seek_end - $seek_start + 1;
-
-      header( 'HTTP/1.1 206 Partial Content' );
-      header( "Content-Range: bytes $range_bytes" );
-    }
-
-    header( 'Accept-Ranges: bytes' );
-    header( "Content-Length: $content_length" );
-    header( "Content-Type: $content_type" );
 
     // If the file doesn't exist, don't count it as a download.
     $bytes_sent = -1;
@@ -287,6 +207,73 @@
 
     // Download succeeded if the total bytes matches or exceeds the file size.
     return $bytes_sent >= $size;
+  }
+
+  /**
+   * Downloads a file, allowing for resuming partial downloads.
+   *
+   * @param string $path Fully qualified path of a file to download.
+   *
+   * @return bool True if the file was transferred.
+   */
+  function download( $path ) {
+    // Don't cache the file stats result.
+    clearstatcache();
+
+    $size = @filesize( $path );
+    $size = $size === false || empty( $size ) ? 0 : $size;
+    $filename = normalize_filename( $path );
+    $content_type = mime_content_type( $filename );
+    $range = "0-$size";
+
+    // Check if a range is sent by browser or download manager.
+    if( isset( $_SERVER[ 'HTTP_RANGE' ] ) ) {
+      list( $units, $range_orig ) = explode( '=', $_SERVER[ 'HTTP_RANGE' ], 2 );
+
+      if( $units == 'bytes' ) {
+        // Multiple ranges could be specified, but only serve the first range.
+        list( $range, $extra_ranges ) = explode( ',', $range_orig, 2 );
+      }
+    }
+
+    // Figure out download piece from range.
+    list( $seek_start, $seek_end ) = explode( '-', $range, 2 );
+
+    // Set start and end based on range, otherwise use defaults.
+    $seek_end = empty( $seek_end )
+      ? max( $size - 1, 0 )
+      : min( abs( $seek_end + 0 ), $size - 1 );
+    $seek_start = empty( $seek_start || $seek_end < abs( $seek_start + 0 ) )
+      ? 0
+      : max( abs( $seek_start + 0 ), 0 );
+
+    header_remove( 'x-powered-by' );
+    header( 'Pragma: public' );
+    header( 'Expires: -1' );
+    header( 'Cache-Control: public, must-revalidate, post-check=0, pre-check=0' );
+    header( 'Cache-Control: private', false );
+    header( "Content-Disposition: attachment; filename=\"$filename\"" );
+    header( 'Content-Transfer-Encoding: binary' );
+
+    $content_length = $size;
+
+    // Send partial content header if downloading a piece (IE workaround).
+    if( $seek_start > 0 || $seek_end < ($size - 1) ) {
+      $range_bytes = $seek_start . '-' . $seek_end . '/' . $size;
+      $content_length = $seek_end - $seek_start + 1;
+
+      header( 'HTTP/1.1 206 Partial Content' );
+      header( "Content-Range: bytes $range_bytes" );
+    }
+
+    header( 'Accept-Ranges: bytes' );
+    header( "Content-Length: $content_length" );
+    header( "Content-Type: $content_type" );
+
+    // Respond to HTTP HEAD requests.
+    return $_SERVER['REQUEST_METHOD'] === 'HEAD'
+      ? false
+      : transmit( $path, $seek_start, $size );
   }
 
   $filename = isset( $_GET[ 'filename' ] ) ? $_GET[ 'filename' ] : '';
